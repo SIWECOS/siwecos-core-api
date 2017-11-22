0845d706dbb45e84e9cd77abd1777d3c05ff9541
Sascha Brendel <code@lednerb.de>
2017-11-22 19:00:14 +0100

    Added more Scanner Jobs

---
+++ CoreApi/app/Jobs/ScanInfoLeakJob.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use GuzzleHttp\Client;
+use App\Scan;
+use App\ScanResult;
+
+class ScanInfoLeakJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $scan;
+    protected $result;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Scan $scan)
+    {
+        $this->scan = $scan;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->runScan();
+        $this->notifyCallbacks();
+    }
+
+    
+    /**
+     * Runs the defined scan and stores the result in the database.
+     *
+     * @return void
+     */
+    public function runScan()
+    {
+        $client = new Client();
+        $response = $client->get(env('INFOLEAK_SCANNER_URL') . '/InfoLeak-Scanner/', [
+            'query' => [
+                'url' => $this->scan->url
+            ]
+        ]);
+        
+        $this->result = $this->scan->results()->create([
+            'scanner_type' => 'infoLeak',
+            'result' => $response->getBody()
+        ]);
+    }
+
+    /**
+     * Sends the ScanResult to the given callback urls.
+     *
+     * @return void
+     */
+    public function notifyCallbacks()
+    {
+        foreach ($this->scan->callbackurls as $callback) {
+            $client = new Client();
+            $client->post($callback, [
+                'http_errors' => false,
+                'body' => $this->result,
+            ]);
+        }
+    }
+}
