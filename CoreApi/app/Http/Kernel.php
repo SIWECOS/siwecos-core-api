e38a0e86459710c6b53bc34453596ac02f270fc2
Marcel Wege <wege@hdsolutions.de>
2017-11-29 13:53:49 +0100

    modified swagger

--- CoreApi/app/Http/Kernel.php
+++ CoreApi/app/Http/Kernel.php
@@ -62,6 +62,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'tokencheck' => TokenCheck::class,
         'creditcheck' => CreditCheck::class,
-        'domaincheck' => DomainCheck::class
+        'domaincheck' => DomainCheck::class,
+        'cors' => \App\Http\Middleware\Cors::class
     ];
 }
