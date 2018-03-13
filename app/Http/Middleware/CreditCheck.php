db7d8446aea07eb6dc354b37abb98cf09a74f359
Marcel Wege <mwege@byte5.de>
2018-03-13 18:20:53 +0100

    Added Translation for details

--- app/Http/Middleware/CreditCheck.php
+++ app/Http/Middleware/CreditCheck.php
@@ -5,26 +5,25 @@ namespace App\Http\Middleware;
 use App\Token;
 use Closure;
 
-class CreditCheck
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        $headerToken = Token::getTokenByString($request->header('siwecosToken'));
-        if ($headerToken instanceof Token)
-        {
-            if ($headerToken->credits > 0)
-            {
-                return $next($request);
-            }
-            return response('Not enough credits', 452);
-        }
-        return response('Token not allowed', 403);
-    }
+class CreditCheck {
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @param  \Closure $next
+	 *
+	 * @return mixed
+	 */
+	public function handle( $request, Closure $next ) {
+		$headerToken = Token::getTokenByString( $request->header( 'siwecosToken' ) );
+		if ( $headerToken instanceof Token ) {
+			if ( $headerToken->credits > 0 ) {
+				return $next( $request );
+			}
+
+			return response( 'Not enough credits', 452 );
+		}
+
+		return response( 'Token not allowed', 403 );
+	}
 }
