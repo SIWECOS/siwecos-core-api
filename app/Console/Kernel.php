04189e2cc4232b03f3f51b189107c36e50760ba4
Marcel Wege <wege@hdsolutions.de>
2018-04-01 09:23:49 +0200

    Changed Cron to 09:30

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-	    $schedule->command(DailyScan::class)->dailyAt('01:00');
+	    $schedule->command(DailyScan::class)->dailyAt('09:30');
     }
 
     /**
