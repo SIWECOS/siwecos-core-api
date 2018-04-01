20fedf4ed876e28ced2210ca1cf002a8e1b63e87
Marcel Wege <wege@hdsolutions.de>
2018-04-01 09:54:45 +0200

    changed timeout

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-	    $schedule->command(DailyScan::class)->dailyAt('10:00');
+	    $schedule->command(DailyScan::class)->hourly();
     }
 
     /**
