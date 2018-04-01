413222ffebd506dfc1006f075dcb11a40cc3337c
Marcel Wege <wege@hdsolutions.de>
2018-04-01 11:42:07 +0200

    Timeout Scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\DailyScan;
+use App\Console\Commands\ScannerTimeout;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -28,6 +29,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
 	    $schedule->command(DailyScan::class)->everyFiveMinutes();
+	    $schedule->command(ScannerTimeout::class)->everyMinute();
     }
 
     /**
