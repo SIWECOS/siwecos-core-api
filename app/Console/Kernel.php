4ea8ef2860c588babf6d3eec7a4a1b79c4e7b59a
Marcel Wege <wege@hdsolutions.de>
2018-04-01 12:23:33 +0200

    Division by 0 in progress

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-	    $schedule->command(DailyScan::class)->everyFiveMinutes();
+	    //$schedule->command(DailyScan::class)->everyFiveMinutes();
 	    $schedule->command(ScannerTimeout::class)->everyMinute();
     }
 
