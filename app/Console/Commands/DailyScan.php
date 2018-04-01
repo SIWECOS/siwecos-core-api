1379579671c161e77d3379e1424a63ca6ff4602c
Marcel Wege <wege@hdsolutions.de>
2018-04-01 18:32:39 +0200

    Cron daily

--- app/Console/Commands/DailyScan.php
+++ app/Console/Commands/DailyScan.php
@@ -3,8 +3,10 @@
 namespace App\Console\Commands;
 
 use App\Domain;
+use App\Scan;
 use App\Http\Controllers\ScanController;
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
 
 class DailyScan extends Command {
 	/**
@@ -40,6 +42,12 @@ class DailyScan extends Command {
 		/** @var Domain $domain */
 		$bar = $this->output->createProgressBar(count($domains));
 		foreach ( $domains as $domain ) {
+            /** @var Scan $latestScan */
+            $latestScan = $domain->scans()->latest()->get();
+		    if ($domain->scans()->latest()->get() > Carbon::now()->addDays(-1)){
+		        Log::info('domain: ' . $domain->domain . ' / last scan: ' . $latestScan->updated_at);
+		        continue;
+            }
 			ScanController::startScanJob( $domain->token, $domain->domain );
 			$this->info('Scan started for: ' . $domain->domain);
 			$bar->advance();
