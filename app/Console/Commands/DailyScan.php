eff61d3e6b83fe263dc5df8c208114393bd92e8f
Marcel Wege <mwege@byte5.de>
2018-04-13 15:34:57 +0200

    Remove Logging of daily scan skip

--- app/Console/Commands/DailyScan.php
+++ app/Console/Commands/DailyScan.php
@@ -45,9 +45,7 @@ class DailyScan extends Command {
 		foreach ( $domains as $domain ) {
             /** @var Scan $latestScan */
             $latestScan = $domain->scans()->latest()->first();
-            Log::info('domain: ' . $domain->domain . ' / last scan: ' . $latestScan->updated_at);
 		    if ($latestScan->updated_at > Carbon::now()->addDays(-1)){
-                Log::info('Skip Domain: ' . $domain->domain);
 		        continue;
             }
 			ScanController::startScanJob( $domain->token, $domain->domain, true );
