d024c2aee287dd0d6993df085ac1afb423c9242a
Marcel Wege <mwege@byte5.de>
2018-06-03 10:21:55 +0200

    Style Fix

--- app/Domain.php
+++ app/Domain.php
@@ -32,110 +32,102 @@ const METATAGNAME = 'siwecostoken';
  * @property \Carbon\Carbon|null $last_notification
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Scan[] $scans
  *
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Domain whereLastNotification($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Domain whereLastNotification( $value )
  */
-class Domain extends Model
-{
-    protected $fillable = ['domain', 'token_id', 'verified', 'domain_token'];
+class Domain extends Model {
+	protected $fillable = [ 'domain', 'token_id', 'verified', 'domain_token' ];
 
-    public function __construct(array $attributes = [])
-    {
-        parent::__construct($attributes);
+	public function __construct( array $attributes = [] ) {
+		parent::__construct( $attributes );
 
-        if (array_key_exists('domain', $attributes)) {
-            $domainFilter = parse_url($attributes['domain']);
-            $this->domain = $domainFilter['scheme'].'://'.$domainFilter['host'];
-        }
-        if (array_key_exists('token', $attributes)) {
-            $token = Token::getTokenByString($attributes['token']);
-            $this->token_id = $token->id;
-            $this->domain_token = Keygen::alphanum(64)->generate();
-        }
-    }
+		if ( array_key_exists( 'domain', $attributes ) ) {
+			$domainFilter = parse_url( $attributes['domain'] );
+			$this->domain = $domainFilter['scheme'] . '://' . $domainFilter['host'];
+		}
+		if ( array_key_exists( 'token', $attributes ) ) {
+			$token              = Token::getTokenByString( $attributes['token'] );
+			$this->token_id     = $token->id;
+			$this->domain_token = Keygen::alphanum( 64 )->generate();
+		}
+	}
 
-    /**
-     * @return bool
-     */
-    public function checkMetatags()
-    {
-    	try{
-		    ini_set('user_agent', 'Mozilla/4.0 (compatible; MSIE 6.0)');
-		    $tags = get_meta_tags($this->domain);
-		    foreach ($tags as $tagkey => $tagvalue) {
-			    if ($tagkey == METATAGNAME) {
-				    if ($tagvalue == $this->domain_token) {
-					    /*Hooray site is activated*/
-					    $this->verified = 1;
-					    $this->save();
+	/**
+	 * @return bool
+	 */
+	public function checkMetatags() {
+		try {
+			ini_set( 'user_agent', 'Mozilla/4.0 (compatible; MSIE 6.0)' );
+			$tags = get_meta_tags( $this->domain );
+			foreach ( $tags as $tagkey => $tagvalue ) {
+				if ( $tagkey == METATAGNAME ) {
+					if ( $tagvalue == $this->domain_token ) {
+						/*Hooray site is activated*/
+						$this->verified = 1;
+						$this->save();
 
-					    return true;
-				    }
-			    }
-		    }
+						return true;
+					}
+				}
+			}
 
-		    return false;
-	    }
-	    catch (\Exception $ex){
-    		return false;
-	    }
+			return false;
+		} catch ( \Exception $ex ) {
+			return false;
+		}
 
-    }
+	}
 
-    public function token()
-    {
-        return $this->belongsTo(Token::class);
-    }
+	public function token() {
+		return $this->belongsTo( Token::class );
+	}
 
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
-    public function scans()
-    {
-        return $this->hasMany(Scan::class, 'url', 'domain');
-    }
+	/**
+	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
+	 */
+	public function scans() {
+		return $this->hasMany( Scan::class, 'url', 'domain' );
+	}
 
-    /**
-     * @return bool
-     */
-    public function checkHtmlPage()
-    {
-        /*get the content of the page. there should be nothing, except the activationkey*/
-        ini_set('user_agent', 'Mozilla/4.0 (compatible; MSIE 6.0)');
-        $url = $this->domain.'/'.$this->domain_token.'.html';
+	/**
+	 * @return bool
+	 */
+	public function checkHtmlPage() {
+		/*get the content of the page. there should be nothing, except the activationkey*/
+		ini_set( 'user_agent', 'Mozilla/4.0 (compatible; MSIE 6.0)' );
+		$url = $this->domain . '/' . $this->domain_token . '.html';
 
-        try {
-            $pageRequest = file_get_contents($url);
-            if ($pageRequest == false) {
-                return false;
-            }
-            if (strpos($pageRequest, $this->domain_token) !== false) {
-                $this->verified = 1;
-                $this->save();
+		try {
+			$pageRequest = file_get_contents( $url );
+			if ( $pageRequest == false ) {
+				return false;
+			}
+			if ( strpos( $pageRequest, $this->domain_token ) !== false ) {
+				$this->verified = 1;
+				$this->save();
 
-                return true;
-            }
+				return true;
+			}
 
-            return false;
-        } catch (\Exception $exception) {
-            Log::warning($exception->getMessage());
+			return false;
+		} catch ( \Exception $exception ) {
+			Log::warning( $exception->getMessage() );
 
-            return false;
-        }
-    }
+			return false;
+		}
+	}
 
-    /**
-     * @param string $domain
-     * @param int    $tokenId
-     *
-     * @return Domain
-     */
-    public static function getDomainOrFail(string $domain, int $tokenId)
-    {
-        Log::warning('DOMAIN: '.$domain.' ID: '.$tokenId);
+	/**
+	 * @param string $domain
+	 * @param int $tokenId
+	 *
+	 * @return Domain
+	 */
+	public static function getDomainOrFail( string $domain, int $tokenId ) {
+		Log::warning( 'DOMAIN: ' . $domain . ' ID: ' . $tokenId );
 
-        $domain = self::where(['domain' => $domain, 'token_id' => $tokenId])->first();
-        if ($domain instanceof self) {
-            return $domain;
-        }
-    }
+		$domain = self::where( [ 'domain' => $domain, 'token_id' => $tokenId ] )->first();
+		if ( $domain instanceof self ) {
+			return $domain;
+		}
+	}
 }
