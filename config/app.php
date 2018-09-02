2b23b67e2dd0e7ed8ef3926dc9e2b2ebdc5d605a
Marcel Wege <mwege@byte5.de>
2018-09-02 19:17:56 +0200

    Remove papertrail

--- config/app.php
+++ config/app.php
@@ -177,7 +177,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         \Barryvdh\Cors\ServiceProvider::class,
-        \App\Providers\PaperTrailServiceProvider::class,
     ],
 
     /*
