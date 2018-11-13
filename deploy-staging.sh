97b375e38adb8bc61094ae116003f0cf91dd1afa
Sascha Brendel <mail@lednerb.eu>
2018-11-13 21:06:13 +0100

    Fixed kubectl command / missing namespace.

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,5 +1,5 @@
 !#/bin/bash
 docker build . -t siwecos/siwecos-core-api:staging
 docker push siwecos/siwecos-core-api:staging
-kubectl patch deployment siwecos-core-api-staging -p \
+kubectl patch deployment siwecos-core-api-staging --namespace staging -p \
   "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
