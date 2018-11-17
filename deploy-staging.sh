5c638971d5d0fbf161c80d0d2151753611cf98c7
Sascha Brendel <mail@lednerb.eu>
2018-11-17 10:18:48 +0100

    Fixed shebang and set executable bit.

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 docker build . -t siwecos/siwecos-core-api:staging
 docker push siwecos/siwecos-core-api:staging
 kubectl patch deployment siwecos-core-api-staging --namespace staging -p \
