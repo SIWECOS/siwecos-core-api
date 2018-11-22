e19b53f83da7988060da1cadeb096f6af25c38ac
Stephan Hradek <github.mails.skeeve@xoxy.net>
2018-11-22 23:57:54 +0100

    accept http status code 3xx as confirmation that domain exists

--- app/Http/Controllers/ScanController.php
+++ app/Http/Controllers/ScanController.php
@@ -132,7 +132,7 @@ class ScanController extends Controller
     }
 
     /**
-     * Check if Domain is Alive or redirects (200 / 301).
+     * Check if Domain is Alive or redirects (200 / 3xx).
      *
      * @param string $domain
      *
@@ -149,7 +149,7 @@ class ScanController extends Controller
 
         try {
             $response = $client->get($domain);
-            if ($response->getStatusCode() === 200 || $response->getStatusCode() === 301) {
+            if ($response->getStatusCode() === 200 || floor($response->getStatusCode() / 100) === 3 ) {
                 return true;
             }
         } catch (\Exception $ex) {
