d311501c6f3e9c2f8d7f4a82578894604add3d6a
Sascha Brendel <mail@lednerb.eu>
2018-12-05 15:36:39 +0100

    Removed unused route.

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,6 @@ Route::prefix('v1')->group(function () {
     Route::middleware(['tokencheck'])->group(function () {
         Route::post('/scan/start', 'ScanController@start')->middleware(['creditcheck', 'domaincheck']);
 
-        Route::get('/scan/result', 'ScanController@result')->middleware('domaincheck');
         Route::get('/scan/result/raw', 'ScanController@resultRaw');
 
         Route::post('/domain/add', 'DomainController@add');
