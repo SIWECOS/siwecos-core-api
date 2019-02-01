5a0ec43d6b917724d1f45b87cf74a2d2784e3344
Marcel Wege <mwege@byte5.de>
2019-02-01 17:53:32 +0000

    Auto stash before merge of "travis" and "origin/develop"

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-!#/bin/bash
-docker build . -t siwecos/siwecos-core-api:dev
-docker push siwecos/siwecos-core-api:dev
-kubectl patch deployment siwecos-core-api -p \
-  "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
